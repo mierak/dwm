3509fc5f30da49d86675cdedad504cfb49078606
MieraK <mrkpwnz@gmail.com>
2022-09-06 00:23:16 +0200

    some more config

--- config.h
+++ config.h
@@ -10,9 +10,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char font[]            = "JetBrainsMono Nerd Font Mono:pixelsize=12:antialias=true:autohint=true:hintstyle=hintfull:style=bold";
-static char dmenufont[]       = "monospace:size=10";
-static const char *fonts[]          = { "FiraCode Nerd Font:size=10:antialias=true:autohint=false", "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true"  };
+static char font[]            = "monospace:size=10";
+static const char *fonts[]          = { font, "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -87,21 +86,21 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 /*
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-		{ "color15",			STRING,  &normfgcolor },
-		{ "color0",				STRING,  &normbgcolor },
 		{ "color0",				STRING,  &normbordercolor },
-		{ "color15",			STRING,  &selfgcolor },
-		{ "color2",				STRING,  &selbgcolor },
-		{ "color2",				STRING,  &selbordercolor },
-		// { "font",               STRING,  &font },
-		{ "dmenufont",          STRING,  &dmenufont },
+		{ "color8",				STRING,  &selbordercolor },
+		{ "color0",				STRING,  &normbgcolor },
+		{ "color4",				STRING,  &normfgcolor },
+		{ "color0",				STRING,  &selfgcolor },
+		{ "color4",				STRING,  &selbgcolor },
+		{ "font",	            STRING,  &font },
+		// { "dmenufont",          STRING,  &dmenufont },
 		{ "borderpx",          	INTEGER, &borderpx },
 		{ "snap",          		INTEGER, &snap },
 		{ "showbar",          	INTEGER, &showbar },
